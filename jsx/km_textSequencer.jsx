91cebcf9d7c89d1a8dc54e28fce0cc82c7a6dea0
KyleHarter <kylenmotion@gmail.com>
2022-06-06 12:13:40 -0400

    Updated textRepeater UI started tabbed panel

--- jsx/km_textSequencer.jsx
+++ jsx/km_textSequencer.jsx
@@ -7,12 +7,10 @@
 */
 
 
-(function km_textRepeater(thisObj) {
+(function km_textSequencer(thisObj) {
     
-    var scriptName = "km_textRepeater"
+    var scriptName = this.name;
     var editcharacters = 20;
-
-
 
     createUI(thisObj);
 
@@ -25,46 +23,33 @@
             });
         
         win.orientation = 'column';
-        win.alignChildren = ["left", "top"];
+        win.alignChildren = ["fill", "top"];
 
-        var textInputGroup = win.add("group", undefined, "text input group");
-        textInputGroup.orientation = 'row';
-        // textInputGroup.alignChildren = ["fill", "fill"];
+        var tpanel = win.add("tabbedpanel");
+        var inputTab = tpanel.add("tab", undefined, "Input");
+        var textInputGroup = inputTab.add("group", undefined, "text input group");
+        textInputGroup.orientation = 'column';
+        textInputGroup.alignChildren = ["fill", "top"];
         var textInputStatic = textInputGroup.add("statictext", undefined, "Text Input:");
-        var textInputEdit = textInputGroup.add("edittext", undefined, "Enter Text Here", {multiline: true, scrollable: true});
+        var textInputEdit = textInputGroup.add("edittext", [0, 0, 150, 100], "Enter Text Here", {multiline: true, scrollable: true});
         textInputEdit.characters = editcharacters;
-        var copiesGroup = textInputGroup.add("group", undefined, "copies group");
-        var numCopiesStatic = copiesGroup.add("statictext", undefined, "# of Copies:");
-        var numCopiesEdit = copiesGroup.add("edittext", undefined, "3");
-        numCopiesEdit.characters = 8;
-
-        // var fontGroup = win.add("group", undefined, "font group");
-        // var fontStatic = fontGroup.add("statictext", undefined, "Pick a font:");
-        // var fontDropdown = fontGroup.add("dropdownList", undefined, []);
-        // fontDropdown.characters = editcharacters;
-
-        var colorCheckGroup = win.add("group", undefined, "color check group");
-        colorCheckGroup.orientation = 'row';
-        var fillStrokeCheckbox = colorCheckGroup.add("checkbox", undefined, "\u00A0Fill/Stroke");
-        fillStrokeCheckbox.value = true;
-        var altColors = colorCheckGroup.add("checkbox", undefined, "\u00A0Alternate Colors");
-
-        var colorInputGroup = win.add("group", undefined, "color input group");
-        colorInputGroup.orientation = 'row';
-        // colorInputGroup.alignChildren = ["fill", "fill"];
-        var mainColorStatic = colorInputGroup.add("statictext", undefined, "Main Color:");
-        var mainColorHexEdit = colorInputGroup.add("edittext", undefined, "ffffff");
-        mainColorHexEdit.characters = 8;
-        var altColorStatic = colorInputGroup.add("statictext", undefined, "Alt Color:");
-        var altColorHexEdit = colorInputGroup.add("edittext", undefined, "d0d0d0");
-        altColorHexEdit.characters = 8;
+        
+        
+        var styleTab = tpanel.add("tab", undefined, "Style");
+        styleTab.orientation = 'column';
+        styleTab.alignChildren = ["fill", "top"];
+        var fontStyle = styleTab.add("panel", undefined, "Font Style");
+        
+        var colorStyle = styleTab.add("panel", undefined, "Color Style");
 
 
+        
         var applyGroup = win.add("group", undefined, "apply group");
         applyGroup.orientation = 'row';
         applyGroup.alignChildren = ["fill", "fill"];
         var helpButton = applyGroup.add("button", undefined, "Help Me");
         var createTextButton = applyGroup.add("button", undefined, "Create Text");
+
 
         helpButton.onClick = function () {
             alert("Help me plz")
