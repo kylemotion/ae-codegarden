f94268ffc1c1b459e3ff5c7062ff21204e733989
KyleHarter <kylenmotion@gmail.com>
2022-06-05 22:29:07 -0400

    Not working right with script ui when docked

--- jsx/km_randomPosition.jsx
+++ jsx/km_randomPosition.jsx
@@ -5,22 +5,7 @@
     var scriptName = "km_randomPosition";
     var editCharacters = 5;
 
-    var currentComp = app.project.activeItem;
-
-    if (!(currentComp && currentComp instanceof CompItem)){
-        alert("Open up a comp first!")
-    return
-    };
-
-    
-    var layerSelection = currentComp.selectedLayers;
-
-    if (layerSelection < 1) {
-        alert("Select atleast 1 layer first!")
-        return
-    };
-
-
+   
     createUI(thisObj)
 
     function createUI(thisObj) {
@@ -32,11 +17,11 @@
             })
     
         win.orientation = 'column';
-        win.alignChildren = ["fill", "fill"];
+        win.alignChildren = ["fill", "top"];
 
 
         var xRow = win.add("group", undefined, "x row");
-        xRow.alignChildren = ["fill", "fill"];
+        xRow.alignChildren = ["left", "top"];
         var minXStatic = xRow.add("statictext", undefined, "Min X: ");
         var minXEdit = xRow.add("edittext", undefined, "0");
         minXEdit.characters = editCharacters;
@@ -51,7 +36,7 @@
         compWidthCheck.onClick = function () { maxXEdit.text = currentComp.width }
 
         var myRow = win.add("group", undefined, "y row");
-        myRow.alignChildren = ["fill", "fill"];
+        myRow.alignChildren = ["left", "top"];
         var minYStatic = myRow.add("statictext", undefined, "Min Y: ");
         var minYEdit = myRow.add("edittext", undefined, "0");
         minYEdit.characters = editCharacters;
@@ -65,7 +50,7 @@
 
         var zRow = win.add("group", undefined, "z row");
         zRow.orientation = 'row';
-        zRow.alignChildren = ["fill", "fill"];
+        zRow.alignChildren = ["left", "top"];
         var minZStatic = zRow.add("statictext", undefined, "Min Z:\u00A0");
         var minZEdit = zRow.add("edittext", undefined, "0");
         minZEdit.characters = editCharacters;
@@ -76,25 +61,38 @@
 
         var bottomRow = win.add("group", undefined, "bottom row");
         bottomRow.orientation = "row";
-        bottomRow.alignChildren = ["fill", "fill"];
+        bottomRow.alignChildren = ["left", "top"];
         var sepDimCheckbox = bottomRow.add("checkbox", undefined, "\u00A0Separate Dimensions");
         sepDimCheckbox.value = false;
         var camCheckbox = bottomRow.add("checkbox", undefined, "\u00A0Add Camera");
 
 
         var sliderRow = win.add("group", undefined, "slider row");
-        sliderRow.alignChildren = ["fill", "fill"];
+        sliderRow.alignChildren = ["left", "top"];
         var sliderStatic = sliderRow.add("statictext", undefined, "Position Randomizer Slider");
         sliderStatic.alignment = "center";
         var slider = sliderRow.add("slider", undefined, 0, 1, 2);
     
-
+        var currentComp = app.project.activeItem;
+        
+        var layerSelection = currentComp.selectedLayers;
+   
 
         slider.onChanging = function () {
-            app.beginUndoGroup("Start positioning")
+            app.beginUndoGroup("Start positioning");
 
+        if (!(currentComp && currentComp instanceof CompItem)) {
+            alert("Open up a comp first!")
+            return
+        };
+
+        if (layerSelection < 1) {
+            alert("Select atleast 1 layer first!")
+            return
+            };
+            
             try {
-                adjustRandomPos(minXEdit.text, maxXEdit.text, minYEdit.text, maxYEdit.text, minZEdit.text, maxZEdit.text);
+                adjustRandomPos(layerSelection, minXEdit.text, maxXEdit.text, minYEdit.text, maxYEdit.text, minZEdit.text, maxZEdit.text);
             } catch (e) {
                 alert(e)
             } finally {
@@ -104,7 +102,19 @@
 
         threeDCheckbox.onClick = function () {
             app.beginUndoGroup("3D Layer");
-            setThreeDLayer(threeDCheckbox.value);
+
+            if (!(currentComp && currentComp instanceof CompItem)) {
+                alert("Open up a comp first!")
+                return
+            };
+
+            if (layerSelection < 1) {
+                alert("Select atleast 1 layer first!")
+                return
+            };
+
+
+            setThreeDLayer(layerSelection, threeDCheckbox.value);
             app.endUndoGroup()
 
         }
@@ -112,84 +122,112 @@
 
         sepDimCheckbox.onClick = function () {
             app.beginUndoGroup("separate dimensions");
-            separateDimensions(sepDimCheckbox.value);
+
+            if (!(currentComp && currentComp instanceof CompItem)) {
+                alert("Open up a comp first!")
+                return
+            };
+
+            if (layerSelection < 1) {
+                alert("Select atleast 1 layer first!")
+                return
+            };
+
+            separateDimensions(layerSelection, sepDimCheckbox.value);
             app.endUndoGroup()
         }
 
         camCheckbox.onClick = function () {
             app.beginUndoGroup("add camera")
+
+            if (!(currentComp && currentComp instanceof CompItem)) {
+                alert("Open up a comp first!")
+                return
+            };
+
+            if (layerSelection < 1) {
+                alert("Select atleast 1 layer first!")
+                return
+            };
+
+
             win.close();
-            addNewCamera(camCheckbox.value, maxZEdit.text) 
+            addNewCamera(currentComp, camCheckbox.value, maxZEdit.text) 
             app.endUndoGroup()
         }
 
 
-        win.layout.layout();
-        win.onResizing = function () {
-            this.layout.resize()
+        win.onResizing = win.onResize = function () {
+            this.layout.resize();
         };
 
-        win.show();
+        if (win instanceof Window) {
+            win.center();
+            win.show();
+        } else {
+            win.layout.layout(true);
+            win.layout.resize();
+        }
     
     }
 
-    function separateDimensions(dimSeparated) {
-        for (var i = 0; i < layerSelection.length; i++) {
-            var pos = layerSelection[i].property("ADBE Transform Group").property("ADBE Position");
+    function separateDimensions(selectedLayers, dimSeparated) {
+        for (var i = 0; i < selectedLayers.length; i++) {
+            var pos = selectedLayers[i].property("ADBE Transform Group").property("ADBE Position");
             if (dimSeparated == true) {
                 pos.dimensionsSeparated = true;
             } else {
                 pos.dimensionsSeparated = false;
             }
         }
-        return layerSelection.length
+        return selectedLayers.length
     }
 
-    function setThreeDLayer(threeDCheck) {
-        for (var i = 0; i < layerSelection.length; i++) {
+    function setThreeDLayer(selectedLayers,threeDCheck) {
+        for (var i = 0; i < selectedLayers.length; i++) {
             
             if (threeDCheck == true) {
-                layerSelection[i].threeDLayer = true;
+                selectedLayers[i].threeDLayer = true;
             } else {
-                layerSelection[i].threeDLayer = false;
+                selectedLayers[i].threeDLayer = false;
             }
         }
-        return layerSelection.length
+        return selectedLayers.length
     }
 
 
-    function adjustRandomPos(xMin, xMax, yMin, yMax, zMin, zMax) {
-        for (var i = 0; i < layerSelection.length; i++) {
+    function adjustRandomPos(selectedLayers,xMin, xMax, yMin, yMax, zMin, zMax) {
+        for (var i = 0; i < selectedLayers.length; i++) {
             var xRange = Math.floor(Math.random() * (parseFloat(xMax) - parseFloat(xMin) + 1)) + parseFloat(xMin);
             var yRange = Math.floor(Math.random() * (parseFloat(yMax) - parseFloat(yMin) + 1)) + parseFloat(yMin);
             var zRange = Math.floor(Math.random() * (parseFloat(zMax) - parseFloat(zMin) + 1)) + parseFloat(zMin);
 
-            var pos = layerSelection[i].property("ADBE Transform Group").property("ADBE Position");
+            var pos = selectedLayers[i].property("ADBE Transform Group").property("ADBE Position");
 
             if (pos.dimensionsSeparated == true) {
-                layerSelection[i].property("ADBE Transform Group").property("ADBE Position_0").setValue(xRange);
-                layerSelection[i].property("ADBE Transform Group").property("ADBE Position_1").setValue(yRange);
+                selectedLayers[i].property("ADBE Transform Group").property("ADBE Position_0").setValue(xRange);
+                selectedLayers[i].property("ADBE Transform Group").property("ADBE Position_1").setValue(yRange);
             } else {
                 pos.setValue([xRange, yRange])
             };
 
-            if (layerSelection[i].threeDLayer == true && pos.dimensionsSeparated == true) {
-                layerSelection[i].property("ADBE Transform Group").property("ADBE Position_0").setValue(xRange);
-                layerSelection[i].property("ADBE Transform Group").property("ADBE Position_1").setValue(yRange);
-                layerSelection[i].property("ADBE Transform Group").property("ADBE Position_3").setValue(zRange);
+            if (selectedLayers[i].threeDLayer == true && pos.dimensionsSeparated == true) {
+                selectedLayers[i].property("ADBE Transform Group").property("ADBE Position_0").setValue(xRange);
+                selectedLayers[i].property("ADBE Transform Group").property("ADBE Position_1").setValue(yRange);
+                selectedLayers[i].property("ADBE Transform Group").property("ADBE Position_3").setValue(zRange);
             } else {
                 pos.setValue([xRange, yRange, zRange])
             } 
 
     }
 
-    return layerSelection.length
+        return selectedLayers.length
     }
 
 
-    function addNewCamera(camCheck, zMax) {
+    function addNewCamera(comp,camCheck, zMax) {
         if (camCheck == true) {
-            var compLayers = currentComp.layers;
+            var compLayers = comp.layers;
             var newCam = compLayers.addCamera("Cam", [960, 540]);
             newCam.property("ADBE Transform Group").property("ADBE Position").setValue([960, 540, -2666.7]);
             var camNull = compLayers.addNull();
