beedac59263009e84cbe9d81b69c492324d3f381
kylemotion <99735799+kylemotion@users.noreply.github.com>
2022-09-12 21:37:35 -0400

    testy

--- jsx/testyTest.jsx
+++ jsx/testyTest.jsx
@@ -1,18 +1,17 @@
 var proj = app.project;
 
 function getMoney(color, name, width, height, pixelAspect){
-    for(var i = 1; i <proj.numitems; i++){
-        /* if(proj.item(i) instanceof CompItem){
-            proj.item(i).layers.addSolid(color,name, width, height, pixelAspect)
-        } */
-        alert("Fuck")
+    for(var i = 0; i < proj.selection.length; i++){
+        if(proj.selection[i] instanceof CompItem){
+            proj.selection[i].layers.addSolid(color,name, width, height, pixelAspect)
+            
+        }
     }
 }
-function getMoneyYes(){
-    alert(proj.numitems)
-}
+
 app.beginUndoGroup("Yolo");
 
-getMoneyYes()
+getMoney([1.0,.6,.3], "Yella", 1920, 1080, 1.0)
+// alert(proj.selection.length)
 
 app.endUndoGroup()
