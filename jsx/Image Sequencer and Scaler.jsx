daf4692454593afefc69657be3408c09261c97cd
KyleHarter <kylenmotion@gmail.com>
2022-09-15 19:26:36 -0400

    fit to comp height

--- jsx/Image Sequencer and Scaler.jsx
+++ jsx/Image Sequencer and Scaler.jsx
@@ -37,7 +37,35 @@ function addImagesToComp() {
 
     for (var n = 0; n < imageSelection.length; n++){
         var addedImages = activeComp.layers.add(imageSelection[n]);
-        // addedImages.
+
+        var layerSize = addedImages.sourceRectAtTime(addedImages.containingComp.time, true);
+        var transProp = addedImages.property("ADBE Transform Group");
+        var imageWidth = layerSize.width;
+        var imageHeight = layerSize.height;
+        var imageScale = transProp.property("ADBE Scale");
+        var smallerDifference = activeComp.height - imageHeight;
+        var largerDifference = imageHeight - activeComp.height;
+
+        if (imageHeight < activeComp.height) {
+            var smallerConversion = ((smallerDifference * 100) / imageHeight);
+            var finalConversion = [0, 100 + smallerConversion, 0];
+        } else if (imageHeight > activeComp.height) {
+            var largerConversion = ((largerDifference * 100) / imageHeight);
+            var finalConversion = [0, 100 - largerConversion, 0];
+        } else {
+            var finalConversion = [100, 100, 100];
+        }
+        
+        imageScale.setValue([finalConversion[1], finalConversion[1], finalConversion[1]]);
+
+        var rangeDuration = activeComp.duration;  
+
+        var imageDuration = rangeDuration / imageSelection.length;
+
+        addedImages.inPoint = (n) * imageDuration;
+        addedImages.outPoint = addedImages.inPoint + imageDuration;
+
+
     }
 
 
