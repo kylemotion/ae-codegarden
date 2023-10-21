e8bec53d228b06633a756cd7d4124c41fa2d9ebe
Kyle Harter <99735799+kylemotion@users.noreply.github.com>
2023-10-21 12:35:03 -0400

    Trim path and temporal update

---
+++ jsx/ZL_addTrimPaths.jsx
@@ -0,0 +1,55 @@
+/**
+ * Adds Trim Paths to selected shape layers, including a keyframe to start and one to end the animation.
+ *
+ * Hold SHIFT when running the script to _not_ add keyframes.
+ *
+ * @author Zack Lovatt <zack@lova.tt>
+ * @version 1.3.1
+ */
+(function addTrimPaths() {
+  var addKeys = !ScriptUI.environment.keyboardState.shiftKey;
+
+  var comp = app.project.activeItem;
+
+  if (!(comp && comp instanceof CompItem)) {
+    alert("Please select a composition!");
+    return;
+  }
+
+  var layers = comp.selectedLayers;
+
+  if (layers.length === 0) {
+    alert("Please select some layers!");
+    return;
+  }
+
+  app.beginUndoGroup("Add Trim Paths");
+
+  for (var ii = 0, il = layers.length; ii < il; ii++) {
+    var layer = layers[ii];
+
+    if (layer.matchName !== "ADBE Vector Layer") {
+      continue;
+    }
+
+    var contents = layer.property("ADBE Root Vectors Group");
+
+    if (!contents.canAddProperty("ADBE Vector Filter - Trim")) {
+      continue;
+    }
+    var trimProp = contents.addProperty("ADBE Vector Filter - Trim");
+
+    if (!addKeys) {
+      continue;
+    }
+
+    var trimEnd = trimProp.property("ADBE Vector Trim End");
+
+    var trimTimes = [layer.inPoint, layer.inPoint + 1];
+    var trimValues = [0, 100];
+
+    trimEnd.setValuesAtTimes(trimTimes, trimValues);
+  }
+
+  app.endUndoGroup();
+})();
---
+++ jsx/ZL_toggleTemporal.jsx
@@ -0,0 +1,45 @@
+/**
+ * Enables temporal continuous status on selected keyframes
+ *
+ * Modifiers:
+ *  - Hold SHIFT to DISABLE temporal continuous
+ *
+ * @author Zack Lovatt <zack@lova.tt>
+ * @version 0.2.2
+ */
+(function toggleTemporalContinuous() {
+  var enable = !ScriptUI.environment.keyboardState.shiftKey;
+
+  var comp = app.project.activeItem;
+
+  if (!(comp && comp instanceof CompItem)) {
+    alert("Please select a composition!");
+    return;
+  }
+
+  var layers = comp.selectedLayers;
+
+  app.beginUndoGroup('Toggle Temporal Continuous');
+
+  for (var ii = 0, il = layers.length; ii < il; ii++) {
+    var layer = layers[ii];
+    var props = layer.selectedProperties;
+
+    for (var jj = 0, jl = props.length; jj < jl; jj++) {
+      var prop = props[jj];
+
+      if (prop instanceof PropertyGroup) {
+        continue;
+      }
+
+      var keys = prop.selectedKeys;
+
+      for (var kk = 0, kl = keys.length; kk < kl; kk++) {
+        var selectedKeyIndex = keys[kk];
+        prop.setTemporalContinuousAtKey(selectedKeyIndex, enable);
+      }
+    }
+  }
+
+  app.endUndoGroup();
+})();
