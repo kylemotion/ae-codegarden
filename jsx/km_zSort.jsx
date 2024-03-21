8f257ba0624145c4c1285588b30cbcb8ed8c0267
Kyle Harter <99735799+kylemotion@users.noreply.github.com>
2024-03-21 18:21:17 -0400

    zort Updated need to target x y pos

--- jsx/km_zSort.jsx
+++ jsx/km_zSort.jsx
@@ -19,58 +19,50 @@
 
 
         var sortDistance = win.add("group", undefined, "Z Sort");
-        sortDistance.alignChildren = ["left", "top"];
+        sortDistance.alignChildren = ["fill", "fill"];
         var zSortStatic = sortDistance.add("statictext", undefined, "Z Sort: ");
         var zSortEdit = sortDistance.add("edittext", undefined, "0");
         zSortEdit.characters = editCharacters;
     
+        var sortGroup = win.add("group", undefined, "sort group");
+        sortGroup.orientation = 'row';
+        sortGroup.alignChildren = ["fill", "fill"];
+        var sortButton = sortGroup.add("button", undefined, "Sort!");
 
+ 
+        sortButton.onClick = function (){
+            
+            
+            var currentComp = app.project.activeItem;
 
- var proj = app.project;
+            if (!(currentComp && currentComp instanceof CompItem)) {
+                alert("Open up a comp first!")
+                return
+            };
 
-function activeComp(){
-    var currentComp = proj.activeItem;
+            if(isNaN(zSortEdit.text)){
+                alert("Enter a number!")
+                return
+            }
 
-    if (!(currentComp && currentComp instanceof CompItem)) {
-        alert("Open up a comp first!")
-        return
-    };
-}
+            if (zSortEdit.text.text == "") {
+                alert("Please enter a valid integer");
+                return
+            }
 
-function selLayer(){
-    var curComp = activeComp();
-    var layerSelection = curComp.selectedLayers;
-
-    var layerArray = new Array();
-
-
-    for(var i = 0; i< layerSelection.length; i++){
-        layerArray.push(layerSelection[i])
-    }
-
-
-        if(layerSelection.length < 1){
-            alert("Select atleast one layer")
-            return 
-        } else {
-            return layerArray
+            win.close();
+            app.beginUndoGroup("z sort")
+            try{
+                sort3D(parseInt(zSortEdit.text))
+            } catch(e) {
+                alert(e)
+            } finally {
+                app.endUndoGroup()
+            }
+            
+            
         }
-
-   
-}
-
-
-function setup3D(zDepth){
-    var layerRange = selLayer();
-
-
-    for(var n = 0; n<layerRange.length; n++){
-        layerRange[n].threeDLayer = true;
-        layerRange[n].property("ADBE Transform Group").property("ADBE Position").setValue([960,540,zDepth*n]);
         
-    }
-    return layerRange
-}
         win.onResizing = win.onResize = function () {
             this.layout.resize();
         };
@@ -82,14 +74,47 @@ function setup3D(zDepth){
             win.layout.layout(true);
             win.layout.resize();
         }
-    
+        
+
+
+
+
+function selLayer(){
+    var layerSelection = app.project.activeItem.selectedLayers;
+    var layerArray = new Array();
+
+
+    for(var i = 0; i< layerSelection.length; i++){
+        layerArray.push(layerSelection[i])
     }
 
 
+    if(layerSelection.length < 1){
+        alert("Select atleast one layer")
+        return 
+    } else {
+        return layerArray
+    }
+
+   
+}
+
+
+function sort3D(z){
+    var layerRange = selLayer();
+var userInput = z;
+    for(var n = 0; n<layerRange.length; n++){
+        layerRange[n].threeDLayer = true;
+        layerRange[n].property("ADBE Transform Group").property("ADBE Position").setValue([value[0],value[1],userInput * n]);
+        
+    }
+    
+    return
+}
 
 
 
-
+} 
 
 })(this)
 
