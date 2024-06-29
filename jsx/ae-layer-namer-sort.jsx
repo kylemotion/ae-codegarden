8a9aae3d71f6d3ae86b5a6cde2a5f28e75e4cd1f
Kyle Harter <99735799+kylemotion@users.noreply.github.com>
2024-06-29 12:12:38 -0400

    v1.0.0 layer naming + sorting. Removed order dropdown. Needs help button

--- jsx/ae-layer-namer-sort.jsx
+++ jsx/ae-layer-namer-sort.jsx
@@ -15,7 +15,7 @@
 **/
 
 
-(function km_layerRenamer(thisObj) {
+(function ae_layerNameSort(thisObj) {
 
     createUI(thisObj);
 
@@ -37,32 +37,45 @@
         var layerNameEditGroup = mainPanel.add("group", undefined, "Layer Name Group");
         layerNameEditGroup.orientation = "column";
         layerNameEditGroup.alignChildren = "left";
-        var layerNameStatic = layerNameEditGroup.add("statictext", undefined, "Layer Name");
+        var layerNameStatic = layerNameEditGroup.add("statictext", undefined, "Layer Name:");
         var layerNameEdit = layerNameEditGroup.add("edittext", undefined, "Enter custom layer name");
         layerNameEdit.characters = 20;
         var separatorGroup = mainPanel.add("group", undefined, "Separator Group");
         separatorGroup.orientation = "column";
         separatorGroup.alignChildren = "left";
-        var separatorStatic = separatorGroup.add("statictext", undefined, "Separator")
+        var separatorStatic = separatorGroup.add("statictext", undefined, "Separator:")
         var separatorEdit = separatorGroup.add("edittext", undefined, "-")
         separatorEdit.characters = 20;
 
         var utilitiesGroup = mainPanel.add("group", undefined);
         utilitiesGroup.orientation = "column";
         utilitiesGroup.alignChildren = "left";
-        var startNumberStatic = utilitiesGroup.add("statictext", undefined, "Start Number");
+        var startNumberStatic = utilitiesGroup.add("statictext", undefined, "Start Number:");
         var startNumberEdit = utilitiesGroup.add("edittext", undefined, "1");
         startNumberEdit.characters = 20;
 
-        var orderDropdownGroup = mainPanel.add("dropdownlist", undefined, ["Ascending", "Descending"]);
-        orderDropdownGroup.selection = "Ascending";
+        // var orderGroup = mainPanel.add("group", undefined, "order sort");
+        // orderGroup.orientation = "column";
+        // orderGroup.alignChildren = ["fill", "top"];
+
+        // var orderDropdownStatic = orderGroup.add("statictext", undefined, "Sort Order:");
+        // var orderDropdown = orderGroup.add("dropdownlist", undefined, ["Ascending", "Descending"]);
+        // orderDropdown.size = [100,25];
+        // orderDropdown.selection = "Ascending";
 
         var buttonGroup = win.add("group", undefined, "buttons");
         buttonGroup.orientation = 'row';
-        buttonGroup.alignChildren = ["fill", "top"];
+        buttonGroup.alignChildren = ["center", "top"];
+
+        var helpButton = buttonGroup.add("button", undefined, "?");
+        helpButton.size = [25,25];
 
         var runScriptButton = buttonGroup.add("button", undefined, "Run Script");
-        runScriptButton.size = [100, 25]
+        runScriptButton.size = [100, 25];
+
+        helpButton.onClick = function(){
+            alert("How to use ae-layer-namer-sort:\r1.")
+        }
 
         
         runScriptButton.onClick = function () {
@@ -76,14 +89,20 @@
                 alert("Please open up a comp first!")
                 return
             }
-            var selectedLayers = activeComp.selectedLayers;
+
+
+            var selectedLayers = getLayerSel(activeComp);
 
             if (selectedLayers.length < 1) {
                 alert("Please select atleast 1 layer first!")
                 return
             }
             
-            renameLayers(selectedLayers,layerNameEdit.text, separatorEdit.text,startNumberEdit.text,iterateOverrideCheckbox.value);
+            var layerRename = renameLayers(selectedLayers,layerNameEdit.text, separatorEdit.text,startNumberEdit.text);
+            var sortedLayers = sortLayers(layerRename);
+
+
+             orderLayersInComp(layerRename,sortedLayers,activeComp)
         }catch(error){
             alert("An error occured on line: " + error.line + "\nError message: " + error.message);
         } finally {
@@ -109,27 +128,31 @@
     
     
 
-    function getLayerSel(){
-        return
+    function getLayerSel(comp){
+        var selLayers = comp.selectedLayers;
+        var selLayersArray = new Array();
+
+        for(var i = 0; i< selLayers.length; i++){
+            selLayersArray.push(selLayers[i])
+        }
+
+
+        return selLayersArray
     }
 
 
 
 
-    function renameLayers(selectedLayers,layerNames, separator, startNum, override) {
+    function renameLayers(selectedLayers,layerNames, separator, startNum) {
 
         for (var i = 0; i < selectedLayers.length; i++) {
             var count = i + 1;
             var startNumCount = Number(startNum) + i;
-            if(override == false){
                 if(!startNum || isNaN(startNum)){
                     selectedLayers[i].name = layerNames + separator + count
                 } else {
                     selectedLayers[i].name = layerNames + separator + startNumCount
                 }
-            } else {
-                selectedLayers[i].name = layerNames
-            }
         }
 
         return selectedLayers
@@ -137,8 +160,29 @@
 
     function sortLayers(layers){
         return layers.slice().sort(function(a,b){
-          return a.index-b.index
+          return a.name-b.name
         })
+      }
+
+
+    function orderLayersInComp(selLayers,layersArray,comp){
+        var origSelLayers = selLayers.reverse();
+        var layerIndices = layersArray;
+        
+        for(var i = 1; i<layerIndices.length; i++){
+          var layer = origSelLayers[i];
+            if(i==0){
+              // alert(layer.name)
+              // Move the first layer to it's new position
+              layer.moveBefore(comp.layer(origSelLayers[origSelLayers.length-1]))
+            } else{
+              // alert(layer.name)
+              // move the rest of the layers
+              layer.moveAfter(origSelLayers[i-1])
+            }
+        }
+  
+        return 
       }
 
 
